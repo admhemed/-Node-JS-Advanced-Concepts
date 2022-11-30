c8ab791b649791d7a6f8673b6944714fb524dff5
Aladdin Mhemed <admhemed@gmail.com>
2022-11-30 12:57:32 +0300

    it seems adding more forks solves the blocking instances of the app

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const app = express();
+const cluster = require("cluster");
+console.log("cluster.isMaster", cluster.isMaster);
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode
+  cluster.fork();
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+} else {
+  // I'm a child, I'm going to act like a server and do nothing else
+  const express = require("express");
+  const app = express();
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {
-
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
   }
+
+  app.get("/", (req, res) => {
+    doWork(15000);
+    res.send("Hi there");
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast!");
+  });
+
+  app.listen(3001);
 }
-
-app.get('/', (req, res) => {
-  doWork(15000)
-  res.send('Hi there');
-});
-
-app.listen(3001);
